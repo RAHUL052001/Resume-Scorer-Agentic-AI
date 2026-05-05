d6ba48e80575197fa4789ea50e75d5be9d05c561
Rahul <rawprac@gmail.com>
2026-05-05 23:10:48 +0530

    second API of the proj

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ app=FastAPI()
 @app.get("/")
 def read_root():
         return {"message": "this is my fastapi tool"}
+
+
+@app.get("/second API")
+def read_func():
+        return {
+                "message":"Second api for the response"
+        }
